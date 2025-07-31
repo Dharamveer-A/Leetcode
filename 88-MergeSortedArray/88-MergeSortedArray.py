8d3392874cedc307cbbf070da08b212be2f311f4
Dharamveer-A <dharamveer4750@gmail.com>
2025-07-31 11:48:04 +0530

    Create 88-MergeSortedArray

---
+++ 88-MergeSortedArray/88-MergeSortedArray.py
@@ -0,0 +1,10 @@
+# Last updated: 31/07/2025, 11:48:03
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        for i in range(len(nums2)):
+            nums1[m+i]=nums2[i]
+        nums1.sort()
+        return nums1
